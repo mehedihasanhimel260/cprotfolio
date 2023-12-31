53a7a7eeb34b0e5f6019d7898e525bfcb8e7579d
mehedihasna260 <mehedihasanhimel260@gmail.com>
2023-12-31 17:33:14 +0600

    first commit

---
+++ resources/views/backend/layout.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('backend.inc.head')
+
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+        <!-- Spinner Start -->
+        <div id="spinner"
+            class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Sidebar Start -->
+        @include('backend.inc.sidebar')
+        <!-- Sidebar End -->
+
+
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+            @include('backend.inc.nav')
+            <!-- Navbar End -->
+
+
+            <!-- Blank Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row vh-100 bg-secondary rounded align-items-center justify-content-center mx-0">
+                    <div class="col-md-6 text-center">
+                        <h3>This is blank page</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Blank End -->
+
+
+            <!-- Footer Start -->
+            @include('backend.inc.footer')
+            <!-- Footer End -->
+        </div>
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    @include('backend.inc.script')
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/admin', function () {
+    return view('backend.layout');
+});
+Route::get('/', function () {
+    return view('frontend.layout');
+});
+
+Auth::routes();
+Route::middleware(['auth'])->group(function () {
+    Route::middleware(['auth', 'user-access:admin'])->group(function () {
+        Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+    });
+
+    Route::middleware(['auth', 'user-access:user'])->group(function () {
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
+    });
+});
+
+/*------------------------------------------
+--------------------------------------------
+All Admin Routes List
+--------------------------------------------
+--------------------------------------------*/
